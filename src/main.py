03cf148667b4de09c1de244e2bf7dd1556261481
Gbagecol <54816222+Gbagecol@users.noreply.github.com>
2022-01-28 22:21:59 -0700

    Added code to set working directory

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ Main script for the password manager.
 """
 
 import userInterface as cli
+import os
 
 
 ##############
@@ -10,6 +11,10 @@ import userInterface as cli
 ##############
 
 if __name__ == "__main__":
+
+    #set cwd to src if it is not already
+    if "src" not in os.getcwd():
+        os.chdir("src")
     
     print()
     print("--------------- WELCOME TO NERPASS ---------------")
